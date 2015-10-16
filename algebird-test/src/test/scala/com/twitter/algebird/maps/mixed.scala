af878acc04a8703e95698ccc379966938930b014
Erik Erlandson <eerlands@redhat.com>
2015-10-16 16:05:02 -0700

    rename to Mixed*

--- algebird-test/src/test/scala/com/twitter/algebird/maps/mixed.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/maps/mixed.scala
@@ -33,15 +33,15 @@ object mixed {
     import com.twitter.algebird.maps.prefixsum.tree._
     import com.twitter.algebird.maps.nearest.tree._
 
-    trait NodeTD[K, V, P] extends NodePS[K, V, P] with NodeInc[K, V] with NodeNearMap[K, V]
+    trait NodeMix[K, V, P] extends NodePS[K, V, P] with NodeInc[K, V] with NodeNearMap[K, V]
 
-    trait LNodeTD[K, V, P] extends NodeTD[K, V, P]
+    trait LNodeMix[K, V, P] extends NodeMix[K, V, P]
       with LNodePS[K, V, P] with LNodeInc[K, V] with LNodeNearMap[K, V]
 
-    trait INodeTD[K, V, P] extends NodeTD[K, V, P]
+    trait INodeMix[K, V, P] extends NodeMix[K, V, P]
       with INodePS[K, V, P] with INodeInc[K, V] with INodeNearMap[K, V] {
-      val lsub: NodeTD[K, V, P]
-      val rsub: NodeTD[K, V, P]
+      val lsub: NodeMix[K, V, P]
+      val rsub: NodeMix[K, V, P]
     }
   }
 
@@ -57,21 +57,21 @@ object mixed {
       val prefixMonoid: IncrementingMonoid[P, V]) {
 
       def iNode(clr: Color, dat: Data[K], ls: Node[K], rs: Node[K]) =
-        new Inject[K, V, P](keyOrdering, valueMonoid, prefixMonoid) with INodeTD[K, V, P] with TDigestMap[K, V, P] {
+        new Inject[K, V, P](keyOrdering, valueMonoid, prefixMonoid) with INodeMix[K, V, P] with MixedMap[K, V, P] {
           // INode[K]
           val color = clr
-          val lsub = ls.asInstanceOf[NodeTD[K, V, P]]
-          val rsub = rs.asInstanceOf[NodeTD[K, V, P]]
+          val lsub = ls.asInstanceOf[NodeMix[K, V, P]]
+          val rsub = rs.asInstanceOf[NodeMix[K, V, P]]
           val data = dat.asInstanceOf[DataMap[K, V]]
           // INodePS[K, V, P]
           val prefix = prefixMonoid.inc(prefixMonoid.plus(lsub.pfs, rsub.pfs), data.value)
           // INodeNear[K, V]
           val kmin = lsub match {
-            case n: INodeTD[K, V, P] => n.kmin
+            case n: INodeMix[K, V, P] => n.kmin
             case _ => data.key
           }
           val kmax = rsub match {
-            case n: INodeTD[K, V, P] => n.kmax
+            case n: INodeMix[K, V, P] => n.kmax
             case _ => data.key
           }
         }
@@ -81,23 +81,23 @@ object mixed {
 
   import infra._
 
-  sealed trait TDigestMap[K, V, P]
-    extends IncrementMapLike[K, V, INodeTD[K, V, P], TDigestMap[K, V, P]]
-    with PrefixSumMapLike[K, V, P, INodeTD[K, V, P], TDigestMap[K, V, P]]
-    with NearestMapLike[K, V, INodeTD[K, V, P], TDigestMap[K, V, P]] {
+  sealed trait MixedMap[K, V, P]
+    extends IncrementMapLike[K, V, INodeMix[K, V, P], MixedMap[K, V, P]]
+    with PrefixSumMapLike[K, V, P, INodeMix[K, V, P], MixedMap[K, V, P]]
+    with NearestMapLike[K, V, INodeMix[K, V, P], MixedMap[K, V, P]] {
 
     override def toString =
-      "TDigestMap(" +
+      "MixedMap(" +
         iterator.zip(prefixSumsIterator())
         .map(x => s"${x._1._1} -> (${x._1._2}, ${x._2})").mkString(", ") +
         ")"
   }
 
-  object TDigestMap {
+  object MixedMap {
     def key[K](implicit num: Numeric[K]) = new AnyRef {
       def value[V](implicit vm: Monoid[V]) = new AnyRef {
-        def prefix[P](implicit im: IncrementingMonoid[P, V]): TDigestMap[K, V, P] =
-          new Inject[K, V, P](num, vm, im) with LNodeTD[K, V, P] with TDigestMap[K, V, P]
+        def prefix[P](implicit im: IncrementingMonoid[P, V]): MixedMap[K, V, P] =
+          new Inject[K, V, P](num, vm, im) with LNodeMix[K, V, P] with MixedMap[K, V, P]
       }
     }
   }
@@ -114,10 +114,10 @@ class MixedMapSpec extends FlatSpec with Matchers {
   import com.twitter.algebird.maps.increment.IncrementMapProperties._
   import com.twitter.algebird.maps.nearest.NearestMapProperties._
 
-  import mixed.TDigestMap
+  import mixed.MixedMap
 
   def mapType1 =
-    TDigestMap.key[Double].value[Int]
+    MixedMap.key[Double].value[Int]
       .prefix(IncrementingMonoid.fromMonoid(implicitly[Monoid[Int]]))
 
   it should "pass randomized tree patterns" in {
